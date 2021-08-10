7f8d9eab670575062b8e40ffb928a73cba206aca
Alastair <alastair@nvision-imaging.com>
2021-08-10 17:27:51 +0200

    added scratch file to play with GRAPE

---
+++ src/scratch.jl
@@ -0,0 +1,72 @@
+
+# basically a scratch file for now as I figure out how I want this to work 
+
+# lets assume that first of all we want to start with simple population transfer
+
+using LinearAlgebra
+# initial state
+ρ0 = [1 0;0 0] .+ 0.0im
+ρT = [0 0;0 1] .+ 0.0im
+
+# we have only sx drive
+const sx = [0 1;1 0] .+ 0.0im
+const sz = [1 0;0 -1] .+ 0.0im
+
+T = 1.0
+dt = 0.1
+N = floor(Int, T/dt)
+k = 1 # number of controls
+
+drive = rand(k, N)
+
+# we should remember that our Hamiltonian is some callable type
+# we need to convert time to index inside right now
+H(drive, t, T, N) = drive[floor(Int, t/T * N)] * sx
+
+# H(t) = H0 + iR  + ∑ₖ c_k(t) H_k
+# N slices in time
+# so control vector is rand(k, N)
+# reshapes control vector into 
+# can then do something similar to Goodwin's thesis and use density matrix vectorisation
+
+c_vec = reshape(drive, (k * N, 1))
+
+# propagator 
+# Phat_n (c_n) = exp( - 1.0im * (H0 + iR + ∑_k c_{k, n} H_k) Δt)
+# where c_n is the matrix representing the coefficients of each control at time 
+
+# H_n is the full Ham at slice n
+# H_k is the control Ham correspondning to k
+
+z = [ρ0]
+for n = 1:N
+    ρ = z[n]
+    H_n = H(drive .* 0.0 .+ 1/2 * pi, n * dt, T, N)
+    H_k = sx
+    blz = zero(H_k)
+    
+    L2 = [H_n H_k; blz H_n]
+    expl2 = exp(-1.0im * L2 * dt)
+
+    DH_k = expl2[1:2, 3:end]
+    P_n = expl2[1:2, 1:2]
+
+    ρ_t = P_n * ρ * P_n'
+    append!(z, [ρ_t])
+
+end
+
+
+zz = [real(tr(i * sz)) for i in z]
+
+H_n = H(drive, 0.1, T, N)
+H_k = sx
+blz = zero(H_k)
+
+
+L2 = [H_n H_k; blz H_n]
+
+expl2 = exp(-1.0im * L2 * dt)
+
+DH_k = expl2[1:2, 3:end]
+P_n = expl2[1:2, 1:2]
