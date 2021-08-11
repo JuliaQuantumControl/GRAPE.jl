e11dfe02e4fafa5b7a2a09ad9b33babd30832d1a
Alastair <alastair@nvision-imaging.com>
2021-08-11 14:03:50 +0200

    almost finished test GRAPE

--- src/scratch.jl
+++ src/scratch.jl
@@ -11,215 +11,117 @@ using LinearAlgebra
 # we have only sx drive
 const sx = [0 1;1 0] .+ 0.0im
 const sz = [1 0;0 -1] .+ 0.0im
+const sy = [0 -1.0im; 1.0im 0]
+const eye2 = I(2)
+
+super_sx = kron(eye2, sx) - kron(sx', eye2)
+super_sy = kron(eye2, sy) - kron(sy', eye2)
 
 T = 1.0
 dt = 0.1
 N = floor(Int, T/dt)
-k = 1 # number of controls
+K = 2 # number of controls
 
 drive = rand(k, N)
+grad = zero(drive)
 
 # we should remember that our Hamiltonian is some callable type
 # we need to convert time to index inside right now
-H(drive, t, T, N) = drive[floor(Int, t/T * N)] * sx
+H(drive, t, T, N) = drive[1, floor(Int, t/T * N),] * sx + drive[2, floor(Int, t/T * N),] * sy
 
-# H(t) = H0 + iR  + ∑ₖ c_k(t) H_k
-# N slices in time
-# so control vector is rand(k, N)
-# reshapes control vector into 
-# can then do something similar to Goodwin's thesis and use density matrix vectorisation
-
-c_vec = reshape(drive, (k * N, 1))
-
-# propagator 
-# Phat_n (c_n) = exp( - 1.0im * (H0 + iR + ∑_k c_{k, n} H_k) Δt)
-# where c_n is the matrix representing the coefficients of each control at time 
-
-# H_n is the full Ham at slice n
-# H_k is the control Ham correspondning to k
-
-z = [ρ0]
-for n = 1:N
-    ρ = z[n]
-    H_n = H(drive .* 0.0 .+ 0.5 * pi, n * dt, T, N)
-    H_k = sx
-    blz = zero(H_k)
-    
-    L2 = [H_n H_k; blz H_n]
-    expl2 = exp(-1.0im * L2 * dt)
-
-    DH_k = expl2[1:2, 3:end]
-    P_n = expl2[1:2, 1:2]
-
-    ρ_t = P_n * ρ * P_n'
-    append!(z, [ρ_t])
-
+function _get_storage_arrays(ψ, ϕ, N_slices, K_controls)
+    dim = size(ψ,1)
+    # you will need a store for both states and costates
+    ψ_store = [zeros(eltype(ψ), size(ψ)) for i = 1:N_slices+1]
+    ϕ_store = [zeros(eltype(ϕ), size(ϕ)) for i = 1:N_slices+1]
+    ψ_store[1] .= ψ
+    ϕ_store[N_slices+1] .= ϕ
+    # will need zeros for the state
+    zerofs = zero(ψ)
+    # and a temp state
+    temp_state = [zerofs; zerofs]
+    # and for the internals of the aux matrix
+    zeromat = zeros(eltype(ψ), (dim, dim))
+    # you also need somewhere to store the auxil matrix
+    aux_mat = [zeromat zeromat; zeromat zeromat]
+    # finally somewhere to store the directional derivative vector
+    directderiv_store = [[zeros(eltype(ψ), size(ψ)) for i = 1:N_slices] for k = 1:K_controls]
+    return dim, ψ_store, ϕ_store, zerofs, temp_state, zeromat, aux_mat, directderiv_store
 end
-
-
-zz = [real(tr(i * sz)) for i in z]
-@show zz
-
-# H_n = H(drive, 0.1, T, N)
-# H_k = sx
-# blz = zero(H_k)
-
-
-# L2 = [H_n H_k; blz H_n]
-
-# expl2 = exp(-1.0im * L2 * dt)
-
-# DH_k = expl2[1:2, 3:end]
-# P_n = expl2[1:2, 1:2]
-
-
-
-# work in vector formalism
-
-ρ0vec = reshape(ρ0, (4,1))
-ρTvec = reshape(ρT, (4, 1))
-
-H_n = H(drive, 0.1, T, N)
-i2 = I(size(H_n,1))
-
-Hhat = kron(i2, H_n) - kron(H_n', i2)
-
-
-z = [ρ0vec]
-
-for n = 1:N
-    ρ = z[n]
-    H_n_temp = H(drive .* 0.0 .+ 0.5 * pi, n * dt, T, N)
-    H_n = kron(i2, H_n_temp) - kron(H_n_temp', i2)
-
-    i2 = I(size(H_n_temp,1))
-
-    H_k = kron(i2, sx) - kron(sx', i2)
-
-    
-    blz = zero(H_k)
-    
-    L2 = [H_n H_k; blz H_n]
-    expl2 = exp(-1.0im * L2 * dt)
-
-    # DH_k = expl2[1:2, 3:end]
-    P_n = expl2[1:4, 1:4]
-
-    ρ_t = P_n * ρ
-    append!(z, [ρ_t])
-
-end
-
-
-szhat = kron(i2, sz) - kron(sz', i2)
-
-zz = [real(tr(sz * reshape(i, (2,2)))) for i in z]
-
-@show zz
-
-# from discussion with Michael
-
-# from notes from goodwin thesis (spinach)
-# do forward prop with arnoldi to compute states going forward
-# do backward prop with arnoldi to compute costates
-# compute fidelity
-# for gradient
-# reuse forward prop states
-# aux matrix to get rhs of derivative
-# then use costates to get gradient value
-
-
-# okay so using Arnoldi from ExpUtils for now
-using ExponentialUtilities
-
-
-
-
-
-
 
 
 ρ0vec = reshape(ρ0, (4))
 ρTvec = reshape(ρT, (4))
 
-H_n = H(drive, 0.1, T, N)
-i2 = I(size(H_n,1))
 
-Hhat = kron(i2, H_n) - kron(H_n', i2)
+dim, ψ_store, ϕ_store, state0, temp_state, mat0, aux_store, dd_store = _get_storage_arrays(ρ0vec, ρTvec, N, K)
 
-# ahead of time we allocate storage arrays for states
-state = [ρ0vec for i=1:N+1]
-costate = [ρTvec for i = 1:N+1]
-# storage array for the directional derivative
-blz = zero(ρ0vec)
-
-# also need to remember the kth index for the states
-directderiv_store = [ρ0vec for i = 1:N+1]
-
+H_K_super = [super_sx, super_sy]
+# forward evolution and directional derivative
 for n = 1:N
-    ρ = state[n]
-    H_n_temp = H(drive .* 0.0 .+ 0.5 * pi, n * dt, T, N)
-    i2 = I(size(H_n_temp,1))
-    H_n = kron(i2, H_n_temp) - kron(H_n_temp', i2)
-
-    H_k = kron(i2, sx) - kron(sx', i2)    
-    blz = zero(H_k)
-
-    ρ_t = expv(-1.0im * dt, H_n, ρ)
-    state[n+1] = ρ_t
-
-    L2 = [H_n H_k; blz H_n]
-    fakestate = [zero(ρ); ρ]
-
-    test = expv(-1.0im * dt, L2, fakestate)
-    directderiv_store[n] = test[1:4]
-    # append!(z, [ρ_t])
+    ψ = ψ_store[n]
+    # could compute Hamiltonian once and store it
+    H_n_temp = H(drive, n * dt, T, N)
+    id = I(size(H_n_temp,1))
     
-    # expl2 = exp(-1.0im * L2 * dt)
+    H_n = kron(id, H_n_temp) - kron(H_n_temp', id)
+    # U * ψ
+    ψ_store[n+1] .= expv(-1.0im * dt, H_n, ψ)
 
-    # # DH_k = expl2[1:2, 3:end]
-    # P_n = expl2[1:4, 1:4]
-
-    # ρ_t = P_n * ρ
+    # compute and store the directional derivative
+    # set up the temp_state 
+    temp_state[dim+1:end] .= ψ
+    for k = 1:K
+        # set up augmented matrix
+        H_k = H_K_super[k]
+        aux_store[1:dim, dim+1:end] .= H_k
+        # then on the diagonal we want H_n
+        aux_store[1:dim, 1:dim] .= H_n
+        aux_store[dim+1:end, dim+1:end] .= H_n
+        dd_store[k][n] .= expv(-1.0im * dt, aux_store, temp_state)[1:dim]
+    end
 end
+
+# now backwards
 
 for n = reverse(1:N)
-    χ = costate[n + 1]
-
-    H_n_temp = H(drive .* 0.0 .+ 0.5 * pi, n * dt, T, N)
-    i2 = I(size(H_n_temp,1))
-    H_n = kron(i2, H_n_temp) - kron(H_n_temp', i2)
-
-    H_k = kron(i2, sx) - kron(sx', i2)    
-    blz = zero(H_k)
-
-    χ_t = expv(1.0im * dt, H_n, χ)
-    costate[n] = χ_t
+    ϕ = ϕ_store[n+1]
+    H_n_temp = H(drive, n * dt, T, N)
+    id = I(size(H_n_temp,1))
+    
+    H_n = kron(id, H_n_temp) - kron(H_n_temp', id)
+    ϕ_store[n] .= expv(1.0im * dt, H_n, ϕ)
 end
 
-# then finally the gradient is computed quite simply using the costates and the directional derivatives
-
-
-
-
-szhat = kron(i2, sz) - kron(sz', i2)
-
-zz = [real(tr(sz * reshape(i, (2,2)))) for i in z]
-
-zzz = [real(tr(sz * reshape(i, (2,2)))) for i in costate]
-
-
-
-struct SchirmerOp
-    H
-    μ
+# compute the fidelity
+fid = abs2(ϕ_store[N]' * ψ_store[N])
+for n = 1:N
+    for k = 1:K
+        grad[k, n] = 2 * real(ϕ_store[n]' * dd_store[k][n] * ψ_store[n]' * ϕ_store[N])
+    end
 end
 
-struct SchirmerState
-    ψ
-    ϕ
-end
+@show "hi"
 
-function mul!(ψ_out::SchirmerState, H::SchirmerOp, ψ::SchirmerState)
+# # ahead of time we allocate storage arrays for states
+# state = [ρ0vec for i=1:N+1]
+# costate = [ρTvec for i = 1:N+1]
+# # storage array for the directional derivative
+# blz = zero(ρ0vec)
 
-end
+# # also need to remember the kth index for the states
+# directderiv_store = [ρ0vec for i = 1:N+1]
+
+# struct SchirmerOp
+#     H
+#     μ
+# end
+
+# struct SchirmerState
+#     ψ
+#     ϕ
+# end
+
+# function mul!(ψ_out::SchirmerState, H::SchirmerOp, ψ::SchirmerState)
+
+# end
