82cd8b81763377198130e04fdeadeb312115cb40
Alastair <alastair@nvision-imaging.com>
2021-08-17 10:12:51 +0200

    commit I forgot about

--- src/optimize.jl
+++ src/optimize.jl
@@ -42,6 +42,59 @@ function _get_storage_arrays(ψ, ϕ, N_slices, K_controls)
     return dim, ψ_store, ϕ_store, temp_state, aux_mat, directderiv_store, H_store
 end
 
-function optimize()
+function optimize(wrk, pulse_options, tlist, propagator, )
+    @unpack objectives, pulse_mapping, H_store, ψ_store, ϕ_store, temp_state,aux_store, dd_store = wrk
+
+    # now we need to make a fn of F, G, x
+    function test_grape(F, G, x, dim, ψ_store, ϕ_store, temp_state, aux_store, dd_store, grad, H_func, H_K_super)
+        for n = 1:N
+            ψ = ψ_store[n]
+            # could compute Hamiltonian once and store it
+            H_store[n] .= H_func(x, n * dt, T, N)
+            
+            # U * ψ
+            ψ_store[n+1] .= expv(-1.0im * dt, H_store[n], ψ)
+        
+            # compute and store the directional derivative
+            # set up the temp_state 
+            temp_state[dim+1:end] .= ψ
+            for k = 1:K
+                # set up augmented matrix
+                H_k = H_K_super[k]
+                aux_store[1:dim, dim+1:end] .= H_k
+                # then on the diagonal we want H_store[n]
+                aux_store[1:dim, 1:dim] .= H_store[n]
+                aux_store[dim+1:end, dim+1:end] .= H_store[n]
+                dd_store[k][n] .= expv(-1.0im * dt, aux_store, temp_state)[1:dim]
+            end
+        end
+    
+        # @show abs2.(ψ_store[end])
+    
+        for n = reverse(1:N)
+            ϕ = ϕ_store[n+1]
+            # H_n_temp = H_func(x, n * dt, T, N)
+            # id = I(size(H_n_temp,1))
+            
+            # H_n = kron(id, H_n_temp) - kron(H_n_temp', id)
+            ϕ_store[n] .= expv(1.0im * dt, H_store[n], ϕ)
+        end
+    
+        fid = abs2(ϕ_store[N]' * ψ_store[N])
+        for n = 1:N
+            for k = 1:K
+                grad[k, n] = 2 * real(ϕ_store[n]' * dd_store[k][n] * ψ_store[n]' * ϕ_store[N])
+            end
+        end
+    
+        if G !== nothing
+            G .= grad
+        end
+        if F !== nothing
+            return fid
+        end
+    
+    
+    end
 
 end
