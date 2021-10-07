b445649310ce099b0eb21f112948d923583d5acc
Michael Goerz <goerz@stanford.edu>
2021-10-07 01:03:23 -0400

    Add GradGenerator, GradVector

--- src/optimize.jl
+++ src/optimize.jl
@@ -4,6 +4,116 @@ using Parameters
 using ConcreteStructs
 using Optim
 
+import LinearAlgebra
+
+
+@doc raw"""Extended generator for the standard dynamic gradient.
+
+```julia
+GG = GradGenerator(G)
+```
+
+contains the original generator `G` (a Hamiltonian or Liouvillian) in `GG.G`, a
+vector of control generators ``∂G/∂ϵᵢ`` (static operators for linear controls,
+and functions for non-linear controls) in `GG.control_generators`, and the
+controls in `GG.controls`.
+
+For a generator ``G = Ĥ = Ĥ₀ + ϵ₁(t) Ĥ₁ + … +  ϵₙ(t) Ĥₙ``, this extended
+generator encodes the block-matrix
+
+```math
+G̃ = \begin{pmatrix}
+    Ĥ      & 0 & \dots  & 0 & Ĥ₁   \\
+    0      & Ĥ & \dots  & 0 & Ĥ₂   \\
+    \vdots &   & \ddots &  & \vdots \\
+    0      & 0 & \dots  & Ĥ & Ĥₙ    \\
+    0      & 0 & \dots  & 0 & Ĥ
+\end{pmatrix}
+```
+"""
+struct GradGenerator{T, CGT, CT}
+    G :: T
+    control_generators :: Vector{CGT}
+    controls :: Vector{CT}
+
+    function GradGenerator(G::T) where T
+        control_generators = G -> [] # TODO implement alongside getcontrols
+        G_ctrl = control_generators(G)
+        controls = getcontrols(G)
+        new{T, eltype(G_ctrl), eltype(controls)}(G, G_ctrl, controls)
+    end
+
+end
+
+
+@doc raw"""Extended state-vector for the dynamic gradient.
+
+```julia
+Ψ̃ = GradVector(Ψ, G̃)
+```
+
+for a state `Ψ` and a [`GradGenerator`](@ref) `G̃` contains the original `Ψ` in
+`Ψ̃.state` and storage for ``n`` gradient-states for the `n` controls in `G̃`
+in `Ψ̃.grad_states`.
+
+The `GradVector` conceptually corresponds to a direct-sum (block) column-vector
+``Ψ̃ = (|Ψ̃₁⟩, |Ψ̃₂⟩, … |Ψ̃ₙ⟩, |Ψ⟩)^T``. With a ``G̃`` as in the documentation of
+[`GradGenerator`](@ref), we have
+
+```math
+G̃ Ψ̃ = \begin{pmatrix}
+Ĥ |Ψ̃₁⟩ + Ĥ₁|Ψ⟩ \\
+\vdots \\
+Ĥ |Ψ̃ₙ⟩ + Ĥₙ|Ψ⟩ \\
+Ĥ |Ψ⟩
+\end{pmatrix}
+```
+
+and
+
+```math
+e^{-i Ĝ dt} \begin{pmatrix} 0 \\ \vdots \\ 0 \\ |Ψ⟩ \end{pmatrix}
+= \begin{pmatrix}
+\frac{∂}{∂ϵ₁} e^{-i Ĥ dt} |Ψ⟩ \\
+\vdots \\
+\frac{∂}{∂ϵₙ} e^{-i Ĥ dt} |Ψ⟩ \\
+e^{-i Ĥ dt} |Ψ⟩
+\end{pmatrix}.
+```
+"""
+struct GradVector{T}
+    state :: T
+    grad_states :: Vector{T}
+
+    GradVector(Ψ::T, G::GradGenerator) where T = new{T}(
+                Ψ, [similar(Ψ) for _ in 1:length(G.control_generators)]
+    )
+
+end
+
+
+function LinearAlgebra.mul!(Φ::GradVector, G::GradGenerator, Ψ::GradVector)
+    LinearAlgebra.mul!(Φ.state, G.G, Ψ.state)
+    for i = 1:length(Ψ.grad_states)
+        LinearAlgebra.mul!(Φ.grad_states[i], G.G, Ψ.grad_states[i])
+        LinearAlgebra.mul!(Φ.grad_states[i], G.control_generators[i], Ψ.state, 1, 1)
+    end
+end
+
+function LinearAlgebra.lmul!(c, Ψ::GradVector)
+    LinearAlgebra.lmul!(c, Ψ.state)
+    for i = 1:length(Ψ.grad_states)
+        LinearAlgebra.lmul!(c, Ψ.grad_states[i])
+    end
+end
+
+
+# TODO: LinearAlgebra.axpy!
+# TODO: LinearAlgebra.norm
+# TODO: LinearAlgebra.dot
+# TODO: copyto!
+
+
 @concrete struct GrapeWrk
     objectives::Any # copy of objectives
     pulse_mapping::Any # as michael describes, similar to c_ops
@@ -13,7 +123,7 @@ using Optim
     aux_state::Any # store for [psi 0]
     aux_store::Any # store for auxiliary matrix
     dP_du::Any # store for directional derivative
-    tlist::Any # tlist 
+    tlist::Any # tlist
     prop_wrk::Any # prop wrk
     aux_prop_wrk::Any # aux prop wrk
 end
