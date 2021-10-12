684ce1d300ab0835879071988c0173e43117534b
Michael Goerz <goerz@stanford.edu>
2021-10-12 00:26:52 -0400

    Move GradGenerator to QuantumControlBase

--- src/optimize.jl
+++ src/optimize.jl
@@ -7,148 +7,6 @@ using Optim
 import LinearAlgebra
 
 
-@doc raw"""Extended generator for the standard dynamic gradient.
-
-```julia
-G̃ = GradGenerator(G)
-```
-
-contains the original generator `G` (a Hamiltonian or Liouvillian) in `G̃.G`, a
-vector of control generators ``∂G/∂ϵᵢ`` (static operators for linear controls,
-and functions for non-linear controls) in `G̃.control_generators`, and the
-controls in `G̃.controls`.
-
-For a generator ``G = Ĥ = Ĥ₀ + ϵ₁(t) Ĥ₁ + … +  ϵₙ(t) Ĥₙ``, this extended
-generator encodes the block-matrix
-
-```math
-G̃ = \begin{pmatrix}
-         Ĥ  &  0 &  \dots   &  0  &  Ĥ₁     \\
-         0  &  Ĥ &  \dots   &  0  &  Ĥ₂     \\
-    \vdots  &    &  \ddots  &     &  \vdots \\
-         0  &  0 &  \dots   &  Ĥ  &  Ĥₙ     \\
-         0  &  0 &  \dots   &  0  &  Ĥ
-\end{pmatrix}
-```
-"""
-struct GradGenerator{T, CGT, CT}
-    G :: T
-    control_generators :: Vector{CGT}
-    controls :: Vector{CT}
-
-    function GradGenerator(G::T) where T
-        control_generators = G -> [] # TODO implement alongside getcontrols
-        G_ctrl = control_generators(G)
-        controls = collect(getcontrols(G))
-        new{T, eltype(G_ctrl), eltype(controls)}(G, G_ctrl, controls)
-    end
-
-end
-
-
-@doc raw"""Extended state-vector for the dynamic gradient.
-
-```julia
-Ψ̃ = GradVector(Ψ, G̃)
-```
-
-for a state `Ψ` and a [`GradGenerator`](@ref) `G̃` contains the original `Ψ` in
-`Ψ̃.state` and storage for ``n`` gradient-states for the `n` controls in `G̃`
-in `Ψ̃.grad_states`.
-
-The `GradVector` conceptually corresponds to a direct-sum (block) column-vector
-``Ψ̃ = (|Ψ̃₁⟩, |Ψ̃₂⟩, … |Ψ̃ₙ⟩, |Ψ⟩)^T``. With a ``G̃`` as in the documentation of
-[`GradGenerator`](@ref), we have
-
-```math
-G̃ Ψ̃ = \begin{pmatrix}
-Ĥ |Ψ̃₁⟩ + Ĥ₁|Ψ⟩ \\
-\vdots \\
-Ĥ |Ψ̃ₙ⟩ + Ĥₙ|Ψ⟩ \\
-Ĥ |Ψ⟩
-\end{pmatrix}
-```
-
-and
-
-```math
-e^{-i G̃ dt} \begin{pmatrix} 0 \\ \vdots \\ 0 \\ |Ψ⟩ \end{pmatrix}
-= \begin{pmatrix}
-\frac{∂}{∂ϵ₁} e^{-i Ĥ dt} |Ψ⟩ \\
-\vdots \\
-\frac{∂}{∂ϵₙ} e^{-i Ĥ dt} |Ψ⟩ \\
-e^{-i Ĥ dt} |Ψ⟩
-\end{pmatrix}.
-```
-"""
-struct GradVector{T}
-    state :: T
-    grad_states :: Vector{T}
-end
-
-function GradVector(Ψ::T, G::GradGenerator) where T
-    # grad_states = [similar(Ψ) for _ in 1:length(G.control_generators)]
-    # for now we need to do this so we don't get a 0D array
-    grad_states = [similar(Ψ) for _ in 1:length(G.controls)]
-    for i = 1 : length(grad_states)
-        LinearAlgebra.lmul!(0.0, grad_states[i])
-    end
-    GradVector{T}(Ψ, grad_states)
-end
-
-
-function LinearAlgebra.mul!(Φ::GradVector, G::GradGenerator, Ψ::GradVector)
-    LinearAlgebra.mul!(Φ.state, G.G, Ψ.state)
-    for i = 1:length(Ψ.grad_states)
-        LinearAlgebra.mul!(Φ.grad_states[i], G.G, Ψ.grad_states[i])
-        LinearAlgebra.mul!(Φ.grad_states[i], G.control_generators[i], Ψ.state, 1, 1)
-    end
-end
-
-
-function LinearAlgebra.lmul!(c, Ψ::GradVector)
-    LinearAlgebra.lmul!(c, Ψ.state)
-    for i = 1:length(Ψ.grad_states)
-        LinearAlgebra.lmul!(c, Ψ.grad_states[i])
-    end
-end
-
-function LinearAlgebra.axpy!(a, X::GradVector, Y::GradVector)
-    LinearAlgebra.axpy(a, X.state, Y.state)
-    for i = 1:length(X.grad_states)
-        LinearAlgebra.axpy(a, X.grad_states[i], Y.grad_states[i])
-    end
-end
-
-
-function LinearAlgebra.norm(Ψ::GradVector)
-    nrm = LinearAlgebra.norm(Ψ.state)
-    for i = 1:length(Ψ.grad_states)
-        nrm += LinearAlgebra.norm(Ψ.grad_states[i])
-    end
-    return nrm
-end
-
-
-function Base.copyto!(dest::GradVector, src::GradVector)
-    copyto!(dest.state, src.state)
-    for i = 1:length(src.grad_states)
-        copyto!(dest.grad_states[i], src.grad_states[i])
-    end
-    return dest
-end
-
-
-function Base.length(Ψ::GradVector)
-    return length(Ψ.state) * (1 + length(Ψ.grad_states))
-end
-
-
-function Base.similar(Ψ::GradVector)
-    return GradVector(similar(Ψ.state), [similar(ϕ) for ϕ ∈ Ψ.grad_states])
-end
-
-
 @concrete struct GrapeWrk
     objectives # copy of objectives
     pulse_mapping # as michael describes, similar to c_ops
@@ -170,13 +28,13 @@ function GrapeWrk(objectives, tlist, prop_method, pulse_mapping = "")
     ψ = initial_state
     # copy from Krotov
     controls = getcontrols(generator)
-    
+
     pulses0 = [discretize_on_midpoints(control, tlist) for control in controls]
-    
+
     N_slices = size(pulses0[1], 1)
-    
+
     zero_vals = IdDict(control => zero(pulses0[i][1]) for (i, control) in enumerate(controls))
-    G = [[setcontrolvals(obj.generator, zero_vals) for i =1:N_slices] for obj in objectives]
+    G = [[evalcontrols(obj.generator, zero_vals) for i =1:N_slices] for obj in objectives]
     vals_dict = [copy(zero_vals) for _ in objectives]
 
     # store for forward evolution
@@ -278,9 +136,9 @@ function optimize(wrk, pulse_options)
             # forward propagate the Schirmer state and use that to extract our forward evolved state
             _fw_prop_w_grad!()
 
-            # compute the fidelity 
+            # compute the fidelity
             τ = real(abs2(ϕ_store[obj][N_slices]' * ψ_store[obj][N_slices]))
-            
+
             # then compute the gradient
             @inbounds for n = 1:N_slices
                 @inbounds for k = 1:N_controls
@@ -301,7 +159,7 @@ function optimize(wrk, pulse_options)
 
     end
 
-    # closure over the variables so that we can optimise 
+    # closure over the variables so that we can optimise
     topt =
         (F, G, x) -> grape_all_obj(
             F,
@@ -339,7 +197,7 @@ function _eval_gen(ϵ, k, n, wrk)
     end
     # will this ever go out of bounds? TODO check
     dt = t[n+1] - t[n]
-    setcontrolvals!(wrk.G[k][n], wrk.objectives[k].generator, vals_dict)
+    evalcontrols!(wrk.G[k][n], wrk.objectives[k].generator, vals_dict)
     return wrk.G[k][n], dt
 end
 
