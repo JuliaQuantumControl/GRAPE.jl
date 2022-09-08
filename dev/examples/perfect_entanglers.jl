39874ddb37f92d9eadaef1ea979b213de7c8f8c2
Documenter.jl <documenter@juliadocs.github.io>
2022-09-08 23:22:07 +0000

    build based on cd4a3e5

--- dev/examples/perfect_entanglers.jl
+++ dev/examples/perfect_entanglers.jl
@@ -162,7 +162,6 @@ J_T_sm(guess_states, objectives)
 
 problem = ControlProblem(
     objectives=objectives,
-    pulse_options=IdDict(),
     tlist=tlist,
     iter_stop=100,
     J_T=J_T_sm,
@@ -231,7 +230,6 @@ J_T_PE(guess_states, objectives)
 
 problem = ControlProblem(
     objectives=objectives,
-    pulse_options=IdDict(),
     tlist=tlist,
     iter_stop=100,
     J_T=J_T_PE,
